4052f759707773dd2c57b1a789ca542007a2e041
rafa <rafarafer@hotmail.com>
2022-02-19 17:51:32 +0100

    Añadimos en move words.java: lagarto y spock.

--- PiedraPapelTijera/src/MoveWords.java
+++ PiedraPapelTijera/src/MoveWords.java
@@ -6,8 +6,8 @@ public class MoveWords{
     public static final int GANA = 1;
     public static final int PIERDE = 2;
 
-    private static final String[] validMoves = {"TIJERAS", "PAPEL", "PIEDRA"};
-    private static final String[] validCommands = {"SALIR", "HELP"};
+    private static final String[] validMoves = {"TIJERAS", "PAPEL", "PIEDRA","LAGARTO","SPOCK"};
+    private static final String[] validCommands = {"SALIR", "AYUDA"};
 
     private Random rnd;
     
